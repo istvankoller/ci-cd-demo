aba46722ae06c2ed1c096a94424b94dd8b8684a4
Bakoczy Zoltan <bakoczy.progmasters@gmail.com>
2023-04-10 20:43:18 +0200

    add supertest

--- server/index.js
+++ server/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const app = require('./server');
 const port = 3000;
-
-app.use('/', (req, res)=> {
-    res.sendFile(getIndexFilePath('index.html'))
-})
-
 
 
 app.listen(port, ()=> {
--- server/utils/util.js
+++
@@ -1,3 +0,0 @@
- const path = require('path')
-
- exports.getIndexFilePath = (file) =>  path.join(__dirname, 'public', file);
